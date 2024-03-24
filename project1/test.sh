d6b40d2bace88c28dfeb1f070e8bd05e62c6d0d1
izthy <908945962@qq.com>
2024-03-24 13:25:33 +0800

    fix buffer overflow

--- project1/test.sh
+++ project1/test.sh
@@ -2,7 +2,7 @@ ID=521021911101
 
 sudo rmmod calc &> /dev/null
 make > /dev/null
-sudo insmod calc.ko operand1=2 operand2=1,2,3,4,5 operator=add
+sudo insmod calc.ko operand1=2 operand2=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 operator=add
 cat /proc/$ID/calc
 
 sudo rmmod calc
